bb773febfaadd57ca6d7f063ef75cc1f17c14f6b
AlBandid <aleksandrakoziol77@gmail.com>
2025-05-14 17:20:55 +0200

    default props and passing functions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function ListItem(props) {
     <li>{props.item}</li>
   )
 }
-
 function SimpleList(props) {
   if(!props.items){
     return <div>Loading...</div>
@@ -26,14 +25,30 @@ function SimpleList(props) {
   )
 }
 
+function Button({ text="Click me!", color="green", fontSize="small", handleClick }) {
+  const buttonStyle = {
+    color,
+    fontSize,
+  };
+
+  return (
+    <button onClick={handleClick} style={buttonStyle}>{text}</button>
+  )
+}
+
 function App() {
   const myList = ["apple pie", "banana bread", "citrus soda", "dragon fruit jelly", "eucalyptus drops"];
+  const renderMessage = (msg) => {
+    console.log(msg)
+  }
 
   return (
     <>
       <h1>Hello world!</h1>
       <Greeting/>
+      <Button handleClick={()=>renderMessage("yay!")}/>
       <SimpleList items={myList} />
+      <Button text="Don't touch it!" color="red" fontSize="big" handleClick={()=>renderMessage("bad user!")} />
       <Farewell/>
     </>
   )
