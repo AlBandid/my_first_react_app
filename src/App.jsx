c3019daa97668d65add0ac5726b04668abd12f35
AlBandid <aleksandrakoziol77@gmail.com>
2025-05-14 16:23:49 +0200

    conditional component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Greeting from './components/Greeting.jsx'
 import { Farewell } from "./components/Farewell.jsx";
 
+function ListItem(props) {
+  return (
+    <li>{props.item}</li>
+  )
+}
+
+function SimpleList(props) {
+  if(!props.items){
+    return <div>Loading...</div>
+  }
+
+  if (props.items.length === 0) {
+    return <h2>No items yet</h2>
+  }
+
+  return (
+    <ul>
+      {props.items.map((item, index) => {
+        return <ListItem key={index} item={item} />
+      })}
+    </ul>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const myList = ["apple pie", "banana bread", "citrus soda", "dragon fruit jelly", "eucalyptus drops"];
 
   return (
     <>
       <h1>Hello world!</h1>
       <Greeting/>
+      <SimpleList items={myList} />
       <Farewell/>
     </>
   )
